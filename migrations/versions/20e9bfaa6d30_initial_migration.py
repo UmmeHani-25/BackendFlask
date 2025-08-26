5dbaf703abec01992f13afde5c5644f2230c7de0
UmmeHani-25 <umme.hani@wanclouds.net>
2025-08-26 15:19:07 +0500

    Changes made for fastapi

--- migrations/versions/20e9bfaa6d30_initial_migration.py
+++ migrations/versions/20e9bfaa6d30_initial_migration.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '20e9bfaa6d30'
-down_revision: Union[str, None] = '36115ad65ed5'
+down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
